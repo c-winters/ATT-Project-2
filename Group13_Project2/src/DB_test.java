324c09e4669dcf102b1ac9031d968e532327bdef
mikeok <mikeok@users.noreply.github.com>
2021-07-09 16:48:20 -0400

    altered the driver assign to beforeall

--- Group13_Project2/src/DB_test.java
+++ Group13_Project2/src/DB_test.java
@@ -9,7 +9,7 @@ public class DB_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// adding a line to test GitHub from mikeok branch
+		// adding a line to test GitHub from mikeok branch this is for Manish
 
 		Connection conn = null;
 		java.sql.Statement stmt = null;
